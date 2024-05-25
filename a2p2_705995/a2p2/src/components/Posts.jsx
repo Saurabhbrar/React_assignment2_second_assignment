e83cb8527b16272c8b8052f30444553c8aeff5b2
Saurabh Brar <saurabh.barar@gmail.com>
2024-05-25 23:01:53 +0530

    Fisrt

---
+++ a2p2_705995/a2p2/src/components/Posts.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Post from "./Post";
+import LoadingIndicator from "./LoadingIndicator";
+import ErrorIndicator from "./ErrorIndicator";
+
+function Posts() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(false);
+
+  async function fetchAndUpdateData() {
+    setLoading(true);
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(response.data);
+      setLoading(false);
+    } catch (error) {
+      setError(true);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchAndUpdateData();
+  }, []);
+
+  if (loading) {
+    return <LoadingIndicator />;
+  }
+
+  if (error) {
+    return <ErrorIndicator />;
+  }
+
+  return (
+    <div>
+      <h1>List of Posts</h1>
+      {posts.map((post) => (
+        <Post {...post} key={post.id} />
+      ))}
+    </div>
+  );
+}
+
+export default Posts;
